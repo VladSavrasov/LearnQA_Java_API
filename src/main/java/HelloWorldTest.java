d8f51106935da4cd34f9ea3b3f7aa748e955e587
Vlad Savrasov <vsvsvs08@rambler.ru>
2022-09-12 15:07:51 +0300

    second commit

--- src/main/java/HelloWorldTest.java
+++ src/main/java/HelloWorldTest.java
@@ -1,12 +1,18 @@
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
 public class HelloWorldTest {
     @Test
     public void testHelloWorld(){
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+    }
+    @Test
+    public void testAgain(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
                 .andReturn();
         response.prettyPrint();
     }
